df3c09948e0f671f9718162b54281427844a6431
macdonc2 <macdonc2@gmail.com>
2020-09-26 17:24:41 -0500

    Minor updates

--- usattelite/loss/loss_metrics.py
+++ usattelite/loss/loss_metrics.py
@@ -89,3 +89,21 @@ class Loss:
         sum_ = K.sum(K.abs(y_true) + K.abs(y_pred), axis=-1)
         jac = (intersection + smooth) / (sum_ - intersection + smooth)
         return (1 - jac) * smooth
+
+    def focal_loss(y_true, y_pred):
+
+        """
+        Focal loss implementation taken from:
+
+        https://www.kdnuggets.com/2018/12/handling-imbalanced-datasets-deep-learning.html
+
+        The hope is that focal loss will help with extremely undersampled classes.  In this case there
+        are some very small objects/classes that require a weighting scheme.
+
+        """
+
+        gamma = 2.0 
+        alpha = 0.25
+        pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+        pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+        return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1))-K.sum((1-alpha) * K.pow( pt_0, gamma) * K.log(1. - pt_0))
